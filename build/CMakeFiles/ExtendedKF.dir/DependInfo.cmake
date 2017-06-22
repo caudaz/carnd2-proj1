0710c30fa44a030fc02b9a6beea027e203b41f40
root <root@p3510.localdomain>
2017-06-21 20:35:10 -0500

    updated FusionEKF.cpp

--- build/CMakeFiles/ExtendedKF.dir/DependInfo.cmake
+++ build/CMakeFiles/ExtendedKF.dir/DependInfo.cmake
@@ -4,10 +4,10 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/mnt/c/UDACITY/TERM2/CarND-Extended-Kalman-Filter-Project/src/FusionEKF.cpp" "/mnt/c/UDACITY/TERM2/CarND-Extended-Kalman-Filter-Project/build/CMakeFiles/ExtendedKF.dir/src/FusionEKF.cpp.o"
-  "/mnt/c/UDACITY/TERM2/CarND-Extended-Kalman-Filter-Project/src/kalman_filter.cpp" "/mnt/c/UDACITY/TERM2/CarND-Extended-Kalman-Filter-Project/build/CMakeFiles/ExtendedKF.dir/src/kalman_filter.cpp.o"
-  "/mnt/c/UDACITY/TERM2/CarND-Extended-Kalman-Filter-Project/src/main.cpp" "/mnt/c/UDACITY/TERM2/CarND-Extended-Kalman-Filter-Project/build/CMakeFiles/ExtendedKF.dir/src/main.cpp.o"
-  "/mnt/c/UDACITY/TERM2/CarND-Extended-Kalman-Filter-Project/src/tools.cpp" "/mnt/c/UDACITY/TERM2/CarND-Extended-Kalman-Filter-Project/build/CMakeFiles/ExtendedKF.dir/src/tools.cpp.o"
+  "/mnt/c/UDACITY/TERM2/term2-proj1/src/FusionEKF.cpp" "/mnt/c/UDACITY/TERM2/term2-proj1/build/CMakeFiles/ExtendedKF.dir/src/FusionEKF.cpp.o"
+  "/mnt/c/UDACITY/TERM2/term2-proj1/src/kalman_filter.cpp" "/mnt/c/UDACITY/TERM2/term2-proj1/build/CMakeFiles/ExtendedKF.dir/src/kalman_filter.cpp.o"
+  "/mnt/c/UDACITY/TERM2/term2-proj1/src/main.cpp" "/mnt/c/UDACITY/TERM2/term2-proj1/build/CMakeFiles/ExtendedKF.dir/src/main.cpp.o"
+  "/mnt/c/UDACITY/TERM2/term2-proj1/src/tools.cpp" "/mnt/c/UDACITY/TERM2/term2-proj1/build/CMakeFiles/ExtendedKF.dir/src/tools.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -20,22 +20,54 @@ FusionEKF::FusionEKF() {
   R_laser_ = MatrixXd(2, 2);
   R_radar_ = MatrixXd(3, 3);
   H_laser_ = MatrixXd(2, 4);
-  Hj_ = MatrixXd(3, 4);
+  Hj_      = MatrixXd(3, 4);
 
   //measurement covariance matrix - laser
-  R_laser_ << 0.0225, 0,
-        0, 0.0225;
+  R_laser_ << 0.0225, 0.0000,
+              0.0000, 0.0225;
 
   //measurement covariance matrix - radar
-  R_radar_ << 0.09, 0, 0,
-        0, 0.0009, 0,
-        0, 0, 0.09;
+  R_radar_ << 0.0900, 0.0000, 0.0000,
+              0.0000, 0.0009, 0.0000,
+              0.0000, 0.0000, 0.0900;
 
   /**
   TODO:
     * Finish initializing the FusionEKF.
     * Set the process and measurement noises
   */
+
+//############################################
+  //create a 4D state vector, we don't know yet the values of the x state
+  ekf_.x_ = VectorXd(4);
+
+  //state covariance matrix P
+  ekf_.P_ = MatrixXd(4, 4);
+  ekf_.P_ << 1, 0,    0,    0,
+             0, 1,    0,    0,
+             0, 0, 1000,    0,
+             0, 0, 0,    1000;
+
+
+  //measurement covariance=
+  // ekf_.R_ = MatrixXd(2, 2);
+  // ekf_.R_ << 0.0225, 0,
+  //      0, 0.0225;
+
+  //measurement matrix
+  H_laser_ << 1, 0, 0, 0,
+              0, 1, 0, 0;
+
+  //the initial transition matrix F_
+  ekf_.F_ = MatrixXd(4, 4);
+  ekf_.F_ << 1, 0, 1, 0,
+             0, 1, 0, 1,
+             0, 0, 1, 0,
+             0, 0, 0, 1;
+
+
+//############################################
+
 
 
 }
@@ -67,11 +99,27 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       /**
       Convert radar from polar to cartesian coordinates and initialize state.
       */
+      //##########################
+      float rho    = measurement_pack.raw_measurements_[0];
+      float phi    = measurement_pack.raw_measurements_[1];
+      float rhodot = measurement_pack.raw_measurements_[2];
+      ekf_.x_ <<    rho*cos(phi),    rho*sin(phi), 
+                 rhodot*cos(phi), rhodot*sin(phi);
+
+      previous_timestamp_ = measurement_pack.timestamp_;
+      //##########################
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
       Initialize state.
       */
+      //#########################
+      float x = measurement_pack.raw_measurements_[0];
+      float y = measurement_pack.raw_measurements_[1];
+      ekf_.x_ << x, y, 0, 0;
+
+      previous_timestamp_ = measurement_pack.timestamp_;
+      //#########################
     }
 
     // done initializing, no need to predict or update
@@ -91,6 +139,31 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
 
+//####################################################################
+  //compute the time elapsed between the current and previous measurements
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0; //dt - expressed in seconds
+  previous_timestamp_ = measurement_pack.timestamp_;
+
+  float dt_2 = dt * dt;
+  float dt_3 = dt_2 * dt;
+  float dt_4 = dt_3 * dt;
+
+  //set the acceleration noise components
+  float noise_ax = 9;
+  float noise_ay = 9;
+  
+  //Modify the F matrix so that the time is integrated
+  ekf_.F_(0, 2) = dt;
+  ekf_.F_(1, 3) = dt;
+
+  //set the process covariance matrix Q
+  ekf_.Q_ = MatrixXd(4, 4);
+  ekf_.Q_ <<  dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
+              0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,
+              dt_3/2*noise_ax, 0, dt_2*noise_ax, 0,
+              0, dt_3/2*noise_ay, 0, dt_2*noise_ay;     
+//###################################################################
+
   ekf_.Predict();
 
   /*****************************************************************************
@@ -105,8 +178,19 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+    //###########################
+    Hj_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.H_ = Hj_;
+    ekf_.R_ = R_radar_;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+    //###########################    
   } else {
     // Laser updates
+    //###########################
+    ekf_.H_ = H_laser_;
+    ekf_.R_ = R_laser_;
+    ekf_.Update(measurement_pack.raw_measurements_);
+    //###########################    
   }
 
   // print the output
